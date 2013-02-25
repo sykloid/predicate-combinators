8ce3ad1a9fca642be0011272a8db4d6e9024d5b6
P.C. Shyamshankar <sykora@lucentbeing.com>
2013-02-25 11:08:37 -0500

    Add the basic is/isn't combinators.

--- src/Data/Function/Predicate.hs
+++ src/Data/Function/Predicate.hs
@@ -1,2 +1,15 @@
 -- | Combinators for working with boolean predicates.
-module Data.Function.Predicate where
+module Data.Function.Predicate (
+    -- * Basic Combinators
+    is,
+    isn't,
+) where
+
+-- | Allows for writing predicates over projection functions rather than the results of those
+-- projections.
+is :: (a -> b) -> (b -> Bool) -> a -> Bool
+is = flip (.)
+
+-- | The negation of @is@, for convenience.
+isn't :: (a -> b) -> (b -> Bool) -> a -> Bool
+isn't = ((.) . (.) . (.)) not is
