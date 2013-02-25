2be83d354851db8bbbbc3087ba9a243c77163d69
P.C. Shyamshankar <sykora@lucentbeing.com>
2013-02-25 11:32:39 -0500

    Add predicate lifting, with lifted conjunction and disjunction.

--- src/Data/Function/Predicate.hs
+++ src/Data/Function/Predicate.hs
@@ -3,7 +3,21 @@ module Data.Function.Predicate (
     -- * Basic Combinators
     is,
     isn't,
+
+    -- * Lifted Combinators
+    (<&&>),
+    (<||>),
+    liftP2
 ) where
+
+import Control.Applicative (liftA2)
+
+-- Fixity declarations for the definitions below. Allows for combining predicates as you would
+-- booleans, without too many parentheses.
+infixr 9 `is`
+infixr 9 `isn't`
+infixr 3 <&&>
+infixr 2 <||>
 
 -- | Allows for writing predicates over projection functions rather than the results of those
 -- projections.
@@ -13,3 +27,15 @@ is = flip (.)
 -- | The negation of @is@, for convenience.
 isn't :: (a -> b) -> (b -> Bool) -> a -> Bool
 isn't = ((.) . (.) . (.)) not is
+
+-- | Lifts a binary function from the result level to the predicate level.
+liftP2 :: (Bool -> Bool -> Bool) -> (a -> Bool) -> (a -> Bool) -> a -> Bool
+liftP2 = liftA2
+
+-- | Lifted conjunction.
+(<&&>) :: (a -> Bool) -> (a -> Bool) -> a -> Bool
+(<&&>) = liftP2 (&&)
+
+-- | Lifted disjunction.
+(<||>) :: (a -> Bool) -> (a -> Bool) -> a -> Bool
+(<||>) = liftP2 (||)
